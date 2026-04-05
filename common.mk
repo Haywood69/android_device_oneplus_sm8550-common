be48da3d2d742c53274db240a734c8666c63818c
Haywood69 <trackingadeadguy@gmail.com>
2026-04-05 16:54:44 -0400

    Revert "sm8550-common: Decommonize UDFPS related parts"
    This reverts commit d9fea98d6b839a0485975e4646c9112d778e30ed.
    
    Change-Id: I256e2c7cf2c5288a8370b8ef34e9e39c58d56d07

--- common.mk
+++ common.mk
@@ -152,6 +152,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
@@ -300,7 +302,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal \
-    sensors.dynamic_sensor_hal
+    sensors.dynamic_sensor_hal \
+    sensors.oplus
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
